18972e3cf43a23f9da17249b9687ba1dc983fbe4
alaa <nima1380620@yahoo.com>
2021-12-27 11:59:16 +0330

    intractive models and component created

---
+++ src/components/tipTapInteractiveInfoTable.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+      v-html="getShortKey"
+  />
+</template>
+
+<script>
+
+import katex from 'katex'
+import {tipTapInteractiveObjects} from '../mixins/tipTapInteractiveObjects'
+
+export default {
+  name: 'InteractiveInfoTable',
+  props: {tipTapInteractiveObjects},
+  computed:{
+    getShortKey(){
+      return katex.renderToString(tipTapInteractiveObjects[0])
+    }
+  },
+  created() {
+    console.log(this.getShortKey)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/mixins/tipTapInteractiveObjects.js
@@ -0,0 +1,203 @@
+export const tipTapInteractiveObjects = [
+    {
+        class: 'english',
+        shortKey: '[Space]',
+        insert: '//enspace'
+    },
+    {
+        class: 'english',
+        shortKey: 'ctrl+[KeyF]',
+        insert: '\\frac{1}{2}'
+    },
+    {
+        class: 'english',
+        shortKey: 'ctrl+alt+[KeyT]]',
+        insert: '#0^\\text{text}'
+    },
+    {
+        class: 'english',
+        shortKey: 'ctrl+alt+[KeyA]',
+        insert: '\\mathop {#0}\\limits^\\Delta'
+    },
+    {
+        class: 'english',
+        shortKey: 'ctrl+[KeyR]',
+        insert: '\\sqrt[]{2}'
+    },
+    {
+        class: 'english',
+        shortKey: 'ctrl+[KeyH]',
+        insert: '2^2'
+    },
+    {
+        class: 'english',
+        shortKey: 'ctrl+[KeyL]',
+        insert: '2_4'
+    },
+    {
+        class: 'english',
+        shortKey: 'ctrl+[KeyJ]',
+        insert: '{2\\atop 1}H'
+    },
+    {
+        class: 'english',
+        shortKey: 'ctrl+[KeyQ]',
+        insert: '//rightarrow'
+    },
+    {
+        class: 'english',
+        shortKey: 'ctrl+[KeyE]',
+        insert: '//theta'
+    },
+    {
+        class: 'english',
+        shortKey: 'ctrl+[KeyS]',
+        insert: '//alpha'
+    },
+    {
+        class: 'english',
+        shortKey: 'ctrl+[KeyD]',
+        insert: '//beta'
+    },
+    {
+        class: 'english',
+        shortKey: 'ctrl+[KeyB]',
+        insert: '//lambda'
+    },
+    {
+        class: 'english',
+        shortKey: 'ctrl+[KeyM]',
+        insert: '//mu'
+    },
+    {
+        class: 'english',
+        shortKey: 'ctrl+[KeyK]',
+        insert: '//rho'
+    },
+    {
+        class: 'english',
+        shortKey: 'ctrl+[KeyU]',
+        insert: '//infty'
+    },
+    {
+        class: 'english',
+        shortKey: 'ctrl+[KeyG]',
+        insert: '//pi'
+    },
+    {
+        class: 'english',
+        shortKey: 'ctrl+alt+[KeyW]',
+        insert: '//Delta'
+    },
+    {
+        class: 'english',
+        shortKey: 'ctrl+alt+[KeyI]',
+        insert: '\\underbrace{#0}_{\\text{note}}'
+    },
+    {
+        class: 'english',
+        shortKey: 'ctrl+alt+[KeyP]',
+        insert: '\\div'
+    },
+
+
+    {
+        class: 'persian',
+        shortKey: 'alt+[Digit0]',
+        insert: '٠'
+    },
+    {
+        class: 'persian',
+        shortKey: '[Numpad0]',
+        insert: '٠'
+    },
+    {
+        class: 'persian',
+        shortKey: 'alt+[Digit1]',
+        insert: '١'
+    },
+    {
+        class: 'persian',
+        shortKey: '[Numpad1]',
+        insert: '١'
+    },
+    {
+        class: 'persian',
+        shortKey: 'alt+[Digit2]',
+        insert: '٢'
+    },
+    {
+        class: 'persian',
+        shortKey: '[Numpad2]',
+        insert: '٢'
+    },
+    {
+        class: 'persian',
+        shortKey: 'alt+[Digit3]',
+        insert: '٣'
+    },
+    {
+        class: 'persian',
+        shortKey: '[Numpad3]',
+        insert: '٣'
+    },
+    {
+        class: 'persian',
+        shortKey: 'alt+[Digit4]',
+        insert: '٤'
+    },
+    {
+        class: 'persian',
+        shortKey: '[Numpad4]',
+        insert: '٤'
+    },
+    {
+        class: 'persian',
+        shortKey: 'alt+[Digit5]',
+        insert: '٥'
+    },
+    {
+        class: 'persian',
+        shortKey: '[Numpad5]',
+        insert: '٥'
+    },
+    {
+        class: 'persian',
+        shortKey: 'alt+[Digit6]',
+        insert: '٦'
+    },
+    {
+        class: 'persian',
+        shortKey: '[Numpad6]',
+        insert: '٦'
+    },
+    {
+        class: 'persian',
+        shortKey: 'alt+[Digit7]',
+        insert: '٧'
+    },
+    {
+        class: 'persian',
+        shortKey: '[Numpad7]',
+        insert: '٧'
+    },
+    {
+        class: 'persian',
+        shortKey: 'alt+[Digit8]',
+        insert: '٨'
+    },
+    {
+        class: 'persian',
+        shortKey: '[Numpad8]',
+        insert: '٨'
+    },
+    {
+        class: 'persian',
+        shortKey: 'alt+[Digit9]',
+        insert: '٩'
+    },
+    {
+        class: 'persian',
+        shortKey: '[Numpad9]',
+        insert: '٩'
+    }]
