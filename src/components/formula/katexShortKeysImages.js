d96fc11605b7fd538d998fbbacbf8372cdcfcabb
alaa <nima1380620@yahoo.com>
2022-01-01 18:12:40 +0330

    katex table done (v 1.0)

---
+++ src/components/formula/katexShortKeysImages.js
@@ -0,0 +1,383 @@
+ export const katexShortKeysImages = [
+    {
+        class: 'singleKey',
+        shortKey: '[Space]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'ctrl+[KeyF]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'tripleKey',
+        shortKey: 'ctrl+alt+[KeyT]]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'tripleKey',
+        shortKey: 'ctrl+alt+[KeyA]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'ctrl+[KeyR]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'ctrl+[KeyH]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'ctrl+[KeyL]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'ctrl+[KeyJ]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'ctrl+[KeyQ]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'ctrl+[KeyE]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'ctrl+[KeyS]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'ctrl+[KeyD]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'ctrl+[KeyB]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'ctrl+[KeyM]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },{
+        class: 'doubleKey',
+        shortKey: 'ctrl+[KeyK]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'ctrl+[KeyU]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'ctrl+[KeyG]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'tripleKey',
+        shortKey: 'ctrl+alt+[KeyW]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'tripleKey',
+        shortKey: 'ctrl+alt+[KeyI]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'tripleKey',
+        shortKey: 'ctrl+alt+[KeyP]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'alt+[Digit0]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'singleKey',
+        shortKey: '[Numpad0]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },{
+        class: 'doubleKey',
+        shortKey: 'alt+[Digit1]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'singleKey',
+        shortKey: '[Numpad1]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'alt+[Digit2]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'singleKey',
+        shortKey: '[Numpad2]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'alt+[Digit3]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'singleKey',
+        shortKey: '[Numpad3]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'alt+[Digit4]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'singleKey',
+        shortKey: '[Numpad4]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'alt+[Digit5]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'singleKey',
+        shortKey: '[Numpad5]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'alt+[Digit6]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'singleKey',
+        shortKey: '[Numpad6]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'alt+[Digit7]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'singleKey',
+        shortKey: '[Numpad7]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'alt+[Digit8]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'singleKey',
+        shortKey: '[Numpad8]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'doubleKey',
+        shortKey: 'alt+[Digit9]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+    {
+        class: 'singleKey',
+        shortKey: '[Numpad9]',
+        imageUrl: [{
+            keyImages: {
+                key1: addKey.num0
+            }
+        }]
+    },
+]
+
+ const imageUrls = [{
+    keys:{
+        ctrl: '',
+        alt: '',
+        num0: 'yow',
+        num1: '',
+        num2: '',
+        num3: '',
+        num4: '',
+        num5: '',
+        num6: '',
+        num7: '',
+        num8: '',
+        num9: '',
+    }
+}]
+   function addKey(){
+            imageUrls.forEach(item => {
+                return  item.keys
+            })
+        }
+
