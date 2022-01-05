d0fa5ef4f69ddbb51ece340a56326e17a22bdba2
neo <neo.kazemi168@gmail.com>
2022-01-05 14:55:00 +0330

    shortkey fix

--- src/components/formula/KatexShortkeys.js
+++ src/components/formula/KatexShortkeys.js
@@ -2,7 +2,7 @@ export const katexShortkeys = [
     {
         class: 'math',
         shortKey: '[Space]',
-        insert: '//enspace'
+        insert: '\\enspace'
     },
     {
         class: 'math',
@@ -42,52 +42,52 @@ export const katexShortkeys = [
     {
         class: 'math',
         shortKey: 'ctrl+[KeyQ]',
-        insert: '//rightarrow'
+        insert: '\\rightarrow'
     },
     {
         class: 'math',
         shortKey: 'ctrl+[KeyE]',
-        insert: '//theta'
+        insert: '\\theta'
     },
     {
         class: 'math',
         shortKey: 'ctrl+[KeyS]',
-        insert: '//alpha'
+        insert: '\\alpha'
     },
     {
         class: 'math',
         shortKey: 'ctrl+[KeyD]',
-        insert: '//beta'
+        insert: '\\beta'
     },
     {
         class: 'math',
         shortKey: 'ctrl+[KeyB]',
-        insert: '//lambda'
+        insert: '\\lambda'
     },
     {
         class: 'math',
         shortKey: 'ctrl+[KeyM]',
-        insert: '//mu'
+        insert: '\\mu'
     },
     {
         class: 'math',
         shortKey: 'ctrl+[KeyK]',
-        insert: '//rho'
+        insert: '\\rho'
     },
     {
         class: 'math',
         shortKey: 'ctrl+[KeyU]',
-        insert: '//infty'
+        insert: '\\infty'
     },
     {
         class: 'math',
         shortKey: 'ctrl+[KeyG]',
-        insert: '//pi'
+        insert: '\\pi'
     },
     {
         class: 'math',
         shortKey: 'ctrl+alt+[KeyW]',
-        insert: '//Delta'
+        insert: '\\Delta'
     },
     {
         class: 'math',
