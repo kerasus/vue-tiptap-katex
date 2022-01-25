f2b428b9dfb0c9931b7e1e6edbbb98f3783d89ac
neo <neo.kazemi168@gmail.com>
2022-01-25 14:47:11 +0330

    paste shortkey change

--- src/extension/Shortkeys/TiptapShortkeys.js
+++ src/extension/Shortkeys/TiptapShortkeys.js
@@ -5,7 +5,7 @@ const Shortkeys = Extension.create({
     addKeyboardShortcuts() {
         return {
             // Paste Shortkey
-            'Mod-Alt-v': () => navigator.clipboard.readText()
+            'Mod-v': () => navigator.clipboard.readText()
                 .then(text => {
                     let string = mixinConvertToTiptap.methods.convertToTiptap(text)
                     this.editor.commands.insertContent(string)
