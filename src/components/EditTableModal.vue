6e1b0e9c1c391a918d7077855ff2b73eefa4170b
JasGH <yasaman.gonehi@gmail.com>
2022-05-21 12:25:06 +0430

    EditTableModal

--- src/components/EditTableModal.vue
+++ src/components/EditTableModal.vue
@@ -4,8 +4,35 @@
       v-model="modal"
       wrapper-class="edit-table-modal-wrapper"
     >
-      <button @click="showSecondModal=true">showSecondModal</button>
-      <sketch-picker v-model="colors" />
+      <div
+        v-if="selected"
+      >
+        <div
+          v-if="selected && selected !== 'backgroundColor'"
+        >
+          <div>currentOptions[selected].cellBorderColor {{ currentOptions[selected].cellBorderColor }}</div>
+          <div>currentOptions[selected].cellBorderWidth {{ currentOptions[selected].cellBorderWidth }}</div>
+          <div>currentOptions[selected].cellBorderType {{ currentOptions[selected].cellBorderType }}</div>
+          <div>currentOptions[selected].none {{ currentOptions[selected].none }}</div>
+        </div>
+        <div v-else>
+          currentOptions.backgroundColor {{ currentOptions.backgroundColor }}
+        </div>
+      </div>
+      <div
+        v-if="selected && selected !== 'backgroundColor'"
+        class="border-color-picker"
+      >
+        <sketch-picker v-model="computedBackgroundColor" />
+        <div>{{ currentOptions[selected].cellBorderColor }}</div>
+      </div>
+      <div
+        v-else
+        class="background-color-picker"
+      >
+        <sketch-picker v-model="computedBackgroundColor" />
+        <div>{{ currentOptions.backgroundColor }}</div>
+      </div>
       <div
         class="cell-box"
         :style="{
@@ -13,26 +40,49 @@
           'border-left': getBorderLeftStyle,
           'border-bottom': getBorderBottomStyle,
           'border-right': getBorderRightStyle,
-          // 'background-color': getBackgroundColor,
-          'width': totalWidth
+          'background-color': computedBackgroundColor
         }"
       >
         your cell
       </div>
-      <button class="Update-btn">
+      <button class="Update-btn" @click="setTableNewStyles">
         Update
       </button>
-      <input
-        v-model="width"
-        type="number"
+      <div
+        v-if="selected && selected !== 'backgroundColor'"
       >
-      <span v-text="width" />
-      <input type="checkbox" id="checkbox" v-model="checked">
-      <label for="checkbox">{{ checked }}</label>
-      <div class="selector-dropdown">
-        <div class="select" style="width: 200px;">
-          <select v-model="borderStyle">
-            <option disabled value="">Please select one</option>
+        <input
+          v-model="currentOptions[selected].cellBorderWidth"
+          type="number"
+        >
+        <span v-text="currentOptions[selected].cellBorderWidth" />
+      </div>
+      <div
+        v-if="selected && selected !== 'backgroundColor'"
+      >
+        <input
+          id="checkbox"
+          v-model="currentOptions[selected].none"
+          type="checkbox"
+        >
+        <label for="checkbox">{{ currentOptions[selected].none }}</label>
+      </div>
+
+      <div
+        v-if="selected && selected !== 'backgroundColor'"
+        class="selector-dropdown cell-border-type"
+      >
+        <div
+          class="select"
+          style="width: 200px;"
+        >
+          <select v-model="currentOptions[selected].cellBorderType">
+            <option
+              disabled
+              value=""
+            >
+              Please select one
+            </option>
             <option>dotted</option>
             <option>dashed</option>
             <option>solid</option>
@@ -41,16 +91,18 @@
             <option>ridge</option>
             <option>inset</option>
             <option>outset</option>
-<!--            <option>none</option>-->
             <option>hidden</option>
             <option>mix</option>
           </select>
           <svg viewBox="0 0 24 24">
-            <path xmlns="http://www.w3.org/2000/svg" d="M10 12.458Q9.833 12.458 9.677 12.396Q9.521 12.333 9.375 12.188L5.604 8.417Q5.354 8.167 5.375 7.792Q5.396 7.417 5.625 7.188Q5.896 6.917 6.25 6.927Q6.604 6.938 6.854 7.188L10 10.354L13.167 7.188Q13.417 6.938 13.76 6.938Q14.104 6.938 14.375 7.208Q14.625 7.458 14.625 7.823Q14.625 8.188 14.375 8.438L10.625 12.188Q10.479 12.333 10.323 12.396Q10.167 12.458 10 12.458Z"/>
+            <path
+              xmlns="http://www.w3.org/2000/svg"
+              d="M10 12.458Q9.833 12.458 9.677 12.396Q9.521 12.333 9.375 12.188L5.604 8.417Q5.354 8.167 5.375 7.792Q5.396 7.417 5.625 7.188Q5.896 6.917 6.25 6.927Q6.604 6.938 6.854 7.188L10 10.354L13.167 7.188Q13.417 6.938 13.76 6.938Q14.104 6.938 14.375 7.208Q14.625 7.458 14.625 7.823Q14.625 8.188 14.375 8.438L10.625 12.188Q10.479 12.333 10.323 12.396Q10.167 12.458 10 12.458Z"
+            />
           </svg>
         </div>
       </div>
-      <div class="selector-dropdown">
+      <div class="selector-dropdown current-options">
         <div
           class="select"
           style="width: 200px;"
@@ -76,26 +128,6 @@
       <div>
         {{ selected }}
       </div>
-    </custom-modal>
-    <custom-modal v-model="showSecondModal" title="Second modal" wrapper-class="edit-table-modal-wrapper">
-      <form novalidate>
-        <div class="form-group">
-          <label for="formField1">Field 1</label>
-          <input id="formField1" type="text" class="form-control" placeholder="" />
-        </div>
-        <div class="form-group">
-          <label for="formField2">Field 2</label>
-          <input id="formField2" type="text" class="form-control" placeholder="" />
-        </div>
-        <div class="row modal-footer">
-          <div class="col-sm-12">
-            <div class="float-right">
-              <button class="btn btn-primary" type="button" @click="showSecondModal = false">Ok</button>
-              <button class="btn btn-secondary ml-2" type="button" @click="showSecondModal = false">Cancel</button>
-            </div>
-          </div>
-        </div>
-      </form>
     </custom-modal>
   </div>
 </template>
@@ -124,27 +156,33 @@ export default {
   data(){
     return {
       colors: '',
-      topBorder: {
-        cellBorderWidth: '8',
-        cellBorderType:'dotted',
-        cellBorderColor:'red'
+      currentOptions : {
+        topBorder: {
+          cellBorderWidth: '2',
+          cellBorderType:'solid',
+          cellBorderColor:'rgba(114, 114, 114, 1)',
+          none: false
+        },
+        rightBorder: {
+          cellBorderWidth: '2',
+          cellBorderType:'solid',
+          cellBorderColor:'rgba(114, 114, 114, 1)',
+          none: false
+        },
+        leftBorder: {
+          cellBorderWidth: '2',
+          cellBorderType:'solid',
+          cellBorderColor:'rgba(114, 114, 114, 1)',
+          none: false
+        },
+        bottomBorder: {
+          cellBorderWidth: '2',
+          cellBorderType:'solid',
+          cellBorderColor:'rgba(114, 114, 114, 1)',
+          none: false
+        },
+        backgroundColor: 'rgba(255, 255, 255, 1)'
       },
-      rightBorder: {
-        cellBorderWidth: '8',
-        cellBorderType:'dotted',
-        cellBorderColor:'yellow'
-      },
-      leftBorder: {
-        cellBorderWidth: '8',
-        cellBorderType:'dotted',
-        cellBorderColor:'blue'
-      },
-      bottomBorder: {
-        cellBorderWidth: '8',
-        cellBorderType:'dotted',
-        cellBorderColor:'pink'
-      },
-      width: 50,
       selected: '',
       options: [
         {
@@ -161,37 +199,30 @@ export default {
         },
         {
           label: 'BorderTop',
-          value: 'borderTop',
+          value: 'topBorder',
           disabled: false,
           selected: false
         },
         {
           label: 'BorderLeft',
-          value: 'borderLeft',
+          value: 'leftBorder',
           disabled: false,
           selected: false
         },
         {
           label: 'BorderBottom',
-          value: 'borderBottom',
+          value: 'bottomBorder',
           disabled: false,
           selected: false
         },
         {
           label: 'BorderRight',
-          value: 'borderRight',
-          disabled: false,
-          selected: false
-        },
-        {
-          label: 'Width',
-          value: 'widthOption',
+          value: 'rightBorder',
           disabled: false,
           selected: false
         }
       ],
       borderStyle: '',
-      showSecondModal: false,
       checked:false
     }
   },
@@ -206,49 +237,59 @@ export default {
     },
     getBorderLeftStyle () {
       if (this.leftBorder) {
-        const border = this.leftBorder
+        const border = this.currentOptions.leftBorder
         return border.cellBorderWidth + 'px ' + border.cellBorderType + ' ' + border.cellBorderColor
       }
       return ''
     },
     getBorderTopStyle () {
       if (this.topBorder && !this.colors.rgba) {
-        const border = this.topBorder
-        return 'dotted' + 'px ' + this.getBackgroundColor + ' ' + border.cellBorderColor
+        const border = this.currentOptions.topBorder
+        return 'dotted' + 'px ' + this.computedBackgroundColor + ' ' + border.cellBorderColor
       }
       return ''
     },
     getBorderRightStyle () {
       if (this.rightBorder) {
-        const border = this.rightBorder
+        const border = this.currentOptions.rightBorder
         return border.cellBorderWidth + 'px ' + border.cellBorderType + ' ' + border.cellBorderColor
       }
       return ''
     },
     getBorderBottomStyle () {
       if (this.bottomBorder) {
-        const border = this.bottomBorder
+        const border = this.currentOptions.bottomBorder
         return border.cellBorderWidth + 'px ' + border.cellBorderType + ' ' + border.cellBorderColor
       }
       return ''
     },
-    getBackgroundColor() {
-      if (!this.colors.rgba){
-        return 'none'
+    computedBackgroundColor: {
+      get () {
+        if (!this.colors.rgba){
+          return 'none'
+        }
+        return 'rgba(' + this.colors.rgba.r+', '+ this.colors.rgba.g+', '+  this.colors.rgba.b+', '+  this.colors.rgba.a+')'
+      },
+      set (value) {
+        const computedVal = 'rgba(' + value.rgba.r+', '+ value.rgba.g+', '+  value.rgba.b+', '+  value.rgba.a+')'
+        if (this.selected && this.selected !== 'backgroundColor') {
+          this.currentOptions[this.selected].cellBorderColor = computedVal
+          return
+        }
+        this.currentOptions.backgroundColor = computedVal
       }
-      return 'rgba(' + this.colors.rgba.r+', '+ this.colors.rgba.g+', '+  this.colors.rgba.b+', '+  this.colors.rgba.a+')'
     },
-    totalWidth() {
-      return this.width+ 'px'
-    }
   },
   methods: {
-    setTableNewStyles () {},
+    setTableNewStyles () {
+      console.log('cellBordersUpdated', this.currentOptions)
+      this.$emit('cellBordersUpdated', this.currentOptions)
+    },
 
   },
   watch:{
     selected(newVal) {
-      console.log('newVal', newVal)
+      // console.log('newVal', newVal)
     }
   }
 }
@@ -266,60 +307,95 @@ export default {
       border-bottom: none;
     }
   }
-  .Update-btn {
-    display: inline-block;
-    outline: 0;
-    cursor: pointer;
-    padding: 5px 16px;
-    font-size: 14px;
-    font-weight: 500;
-    line-height: 20px;
-    vertical-align: middle;
-    border: 1px solid;
-    border-radius: 6px;
-    color: #ffffff;
-    background-color: #2ea44f;
-    border-color: #1b1f2326;
-    box-shadow: rgba(27, 31, 35, 0.04) 0px 1px 0px 0px, rgba(255, 255, 255, 0.25) 0px 1px 0px 0px inset;
-    transition: 0.2s cubic-bezier(0.3, 0, 0.5, 1);
-    transition-property: color, background-color, border-color;
-  }
-  .Update-btn:hover {
-    background-color: #2c974b;
-    border-color: #1b1f2326;
-    transition-duration: 0.1s;
-  }
-  .cell-box {}
-  .selector-dropdown {
-    .select {
-      position: relative;
 
-      select {
-        -webkit-appearance: none;
-        outline: 0;
-        border: 0;
-        display: block;
-        width: 100%;
-        padding: 0px 15px;
-        height: 3rem;
-        font-size: 1rem;
-        font-weight: 400;
-        background: #F2F2F2;
-        border-radius: 16px;
+    .cell-box {}
+    .selector-dropdown {
+      .select {
+        position: relative;
+
+        select {
+          -webkit-appearance: none;
+          outline: 0;
+          border: 0;
+          display: block;
+          width: 100%;
+          padding: 0px 15px;
+          height: 3rem;
+          font-size: 1rem;
+          font-weight: 400;
+          background: #F2F2F2;
+          border-radius: 16px;
+        }
+
+        svg {
+          position: absolute;
+          right: 5px;
+          top: 5px;
+          bottom: 0;
+          margin: auto;
+          height: 1.5rem;
+          width: 1.5rem;
+          fill: #757577;
+          pointer-events: none;
+        }
       }
 
-      svg {
-        position: absolute;
-        right: 5px;
-        top: 5px;
-        bottom: 0;
-        margin: auto;
-        height: 1.5rem;
-        width: 1.5rem;
-        fill: #757577;
-        pointer-events: none;
-      }
     }
+</style>
+<style scoped lang="scss">
+.Update-btn {
+  display: inline-block;
+  outline: 0;
+  cursor: pointer;
+  padding: 5px 16px;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+  vertical-align: middle;
+  border: 1px solid;
+  border-radius: 6px;
+  color: #ffffff;
+  background-color: #2ea44f;
+  border-color: #1b1f2326;
+  box-shadow: rgba(27, 31, 35, 0.04) 0px 1px 0px 0px, rgba(255, 255, 255, 0.25) 0px 1px 0px 0px inset;
+  transition: 0.2s cubic-bezier(0.3, 0, 0.5, 1);
+  transition-property: color, background-color, border-color;
+}
+.Update-btn:hover {
+  background-color: #2c974b;
+  border-color: #1b1f2326;
+  transition-duration: 0.1s;
+}
+.selector-dropdown {
+.select {
+  position: relative;
 
-  }
+select {
+  -webkit-appearance: none;
+  outline: 0;
+  border: 0;
+  display: block;
+  width: 100%;
+  padding: 0px 15px;
+  height: 3rem;
+  font-size: 1rem;
+  font-weight: 400;
+  background: #F2F2F2;
+  border-radius: 16px;
+}
+
+svg {
+  position: absolute;
+  right: 5px;
+  top: 5px;
+  bottom: 0;
+  margin: auto;
+  height: 1.5rem;
+  width: 1.5rem;
+  fill: #757577;
+  pointer-events: none;
+}
+}
+
+}
 </style>
